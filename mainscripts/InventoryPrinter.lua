1e98927ba451c38225888096a7730be2b7a17071
Imrankhan1223 <ImranK1223@gmail.com>
2024-09-10 18:44:30 -0700

    Update InventoryPrinter.lua

--- mainscripts/InventoryPrinter.lua
+++ mainscripts/InventoryPrinter.lua
@@ -11,9 +11,9 @@ G2L["2"] = Instance.new("Frame")
 G2L["2"].Parent = G2L["1"]
 G2L["2"].BorderSizePixel = 0
 G2L["2"].BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-G2L["2"].Size = UDim2.new(0, 307, 0, 264)
+G2L["2"].Size = UDim2.new(0, 184, 0, 165)
 G2L["2"].BorderColor3 = Color3.fromRGB(0, 0, 0)
-G2L["2"].Position = UDim2.new(0.462, 0, 0.236, 0)
+G2L["2"].Position = UDim2.new(0.5954347252845764, 0, 0.35209235548973083, 0);
 G2L["2"].Name = "MainFrame"
 G2L["2"].Draggable = true
 
@@ -26,10 +26,12 @@ G2L["3"].TextSize = 14
 G2L["3"].BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 G2L["3"].TextColor3 = Color3.fromRGB(0, 0, 0)
 G2L["3"].FontFace = Font.new("rbxasset://fonts/families/SourceSansPro.json", Enum.FontWeight.Regular, Enum.FontStyle.Normal)
-G2L["3"].Size = UDim2.new(0, 307, 0, 233)
+G2L["3"].Size = UDim2.new(0, 184, 0, 137)
 G2L["3"].BorderColor3 = Color3.fromRGB(0, 0, 0)
 G2L["3"].Text = ""
 G2L["3"].Name = "ExportWindow"
+G2L["3"].Multiline = true
+G2L["3"].ClearTextOnFocus = true
 
 -- Create the RefreshButton
 G2L["4"] = Instance.new("TextButton")
@@ -39,11 +41,11 @@ G2L["4"].BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 G2L["4"].TextSize = 14
 G2L["4"].FontFace = Font.new("rbxasset://fonts/families/SourceSansPro.json", Enum.FontWeight.Regular, Enum.FontStyle.Normal)
 G2L["4"].TextColor3 = Color3.fromRGB(0, 0, 0)
-G2L["4"].Size = UDim2.new(0, 36, 0, 19)
+G2L["4"].Size = UDim2.new(0, 20, 0, 20)
 G2L["4"].Name = "RefreshButton"
 G2L["4"].BorderColor3 = Color3.fromRGB(0, 0, 0)
 G2L["4"].Text = "🔃"
-G2L["4"].Position = UDim2.new(0.883, 0, 0.928, 0)
+G2L["4"].Position = UDim2.new(0.7251275181770325, 0, -0.1515151560306549, 0);
 
 -- Create the TextLabel
 G2L["5"] = Instance.new("TextLabel")
@@ -53,10 +55,10 @@ G2L["5"].BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 G2L["5"].FontFace = Font.new("rbxasset://fonts/families/SourceSansPro.json", Enum.FontWeight.Regular, Enum.FontStyle.Normal)
 G2L["5"].TextSize = 14
 G2L["5"].TextColor3 = Color3.fromRGB(0, 0, 0)
-G2L["5"].Size = UDim2.new(0, 271, 0, 31)
+G2L["5"].Size = UDim2.new(0, 162, 0, 17)
 G2L["5"].BorderColor3 = Color3.fromRGB(0, 0, 0)
 G2L["5"].Text = "Inventory Exporter by imrooli"
-G2L["5"].Position = UDim2.new(0, 0, 0.883, 0)
+G2L["5"].Position = UDim2.new(0.05978260934352875, 0, 0.8916666507720947, 0);
 
 -- Create the ToggleFrame
 G2L["6"] = Instance.new("Frame")
@@ -65,7 +67,7 @@ G2L["6"].BorderSizePixel = 0
 G2L["6"].BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 G2L["6"].Size = UDim2.new(0, 20, 0, 19)
 G2L["6"].BorderColor3 = Color3.fromRGB(0, 0, 0)
-G2L["6"].Position = UDim2.new(0.611, 0, 0.236, 0)
+G2L["6"].Position = UDim2.new(0.7703201770782471, 0, 0.32285940647125244, 0);
 G2L["6"].Name = "ToggleFrame"
 
 -- Create the ToggleButton
@@ -83,7 +85,7 @@ G2L["7"].Text = "❌"
 
 -- Define variables for easier access
 local player = game:GetService("Players").LocalPlayer
-local gui = player.PlayerGui:FindFirstChild("InventoryExporterGUI")
+local gui = player.PlayerGui:FindFirstChild("ScreenGui")
 local HttpService = game:GetService("HttpService")
 
 -- Check GUI elements
